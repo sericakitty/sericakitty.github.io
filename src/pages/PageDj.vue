0f98b260e8c8b6ae1d8365a11dd354e153925fdb
sericakitty <serica.suhonen@hotmail.com>
2023-04-18 23:57:01 +0300

    up

--- src/pages/PageDj.vue
+++ src/pages/PageDj.vue
@@ -1,53 +1,50 @@
 <template>
-	<section>
-		<div class='text-center'>
-			<a href='#events'>GO TO THE GIG LIST</a>
-		</div>
-		<TheDjPageText />
-		<h2 id='events'>Events that I've hosted with my friends and with my sister <a href='https://www.mixcloud.com/angelicaroselie/' target='_blank'>Angelica Roselie</a>.</h2>
-		<div class='events'>
-			<TheEventList :events='events' />
-		</div>
+  <section>
+    <div class='text-center'>
+      <a href='#events'>GO TO THE GIG LIST</a>
+    </div>
+    <TheDjPageText />
+    <h2 id='events'>Events that I've hosted with my friends and with my sister <a href='https://www.mixcloud.com/angelicaroselie/' target='_blank'>Angelica Roselie</a>.</h2>
+    <div class='events'>
+      <TheEventList :events='events' />
+    </div>
 
-		<h2>Gigs Calendar</h2>
-		<div class='gigs'>
-			<TheGigList :gigs='gigs' />
-		</div>
-	</section>
+    <h2>Gigs Calendar</h2>
+    <div class='gigs'>
+      <TheGigList :gigs='gigs' />
+    </div>
+  </section>
 </template>
 
 <script>
-	import TheEventList from '@/components/TheEventList.vue';
-	import TheGigList from '@/components/TheGigList.vue';
-	import TheDjPageText from '@/components/TheDjPageText.vue';
-	import axios from 'axios';
+import TheEventList from '@/components/TheEventList.vue';
+import TheGigList from '@/components/TheGigList.vue';
+import TheDjPageText from '@/components/TheDjPageText.vue';
+import axios from 'axios';
 
-	const dbURL = './db.json';
-
-	export default {
-		data() {
-			return {
-				gigs: [],
-				events: [],
-			};
-		},
-		components: {
-			TheGigList,
-			TheEventList,
-			TheDjPageText,
-		},
-		async created() { // created hook, making ajax call. async so we are waiting response
-			try {
-				const response = await axios.get(dbURL); // using axios.get method() to make html request to get data from data.json
-				this.gigs = response.data.gigs;
-				this.events = response.data.events;
-			} catch (err) {
-				console.log(err);
-			}
-		},
-	};
+export default {
+  data() {
+    return {
+      BASEURI: './db.json',
+      gigs: [],
+      events: [],
+    };
+  },
+  components: {
+    TheGigList,
+    TheEventList,
+    TheDjPageText,
+  },
+  async created() { // created hook, making ajax call. async so we are waiting response
+    try {
+      const response = await axios.get(this.BASEURI); // using axios.get method() to make html request to get data from data.json
+      this.gigs = response.data.gigs;
+      this.events = response.data.events;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
