e465a43f7b497983c421d3e2cedf7d9b65a0763d
sericakitty <serica.suhonen@hotmail.com>
2023-04-19 00:26:21 +0300

    update created function

--- src/pages/PageDj.vue
+++ src/pages/PageDj.vue
@@ -5,13 +5,20 @@
     </div>
     <TheDjPageText />
     <h2 id='events'>Events that I've hosted with my friends and with my sister <a href='https://www.mixcloud.com/angelicaroselie/' target='_blank'>Angelica Roselie</a>.</h2>
-    <div class='events'>
-      <TheEventList :events='events' />
-    </div>
 
-    <h2>Gigs Calendar</h2>
-    <div class='gigs'>
-      <TheGigList :gigs='gigs' />
+    <div v-if='gigs && events'>
+
+      <div class='events'>
+        <TheEventList :events='events' />
+      </div>
+
+      <h2>Gigs Calendar</h2>
+      <div class='gigs'>
+        <TheGigList :gigs='gigs' />
+      </div>
+    </div>
+    <div v-else>
+      <p class='text-center'>Loading gigs and events...</p>
     </div>
   </section>
 </template>
@@ -22,10 +29,11 @@ import TheGigList from '@/components/TheGigList.vue';
 import TheDjPageText from '@/components/TheDjPageText.vue';
 import axios from 'axios';
 
+const BASEURI = 'http://localhost:3000';
+
 export default {
   data() {
     return {
-      BASEURI: './db.json',
       gigs: [],
       events: [],
     };
@@ -37,9 +45,19 @@ export default {
   },
   async created() { // created hook, making ajax call. async so we are waiting response
     try {
-      const response = await axios.get(this.BASEURI); // using axios.get method() to make html request to get data from data.json
-      this.gigs = response.data.gigs;
-      this.events = response.data.events;
+      const response = await axios.get(`${BASEURI}/gigs`)
+      if (response.status === 200) {
+        this.gigs = response.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    try {
+      const response = await axios.get(`${BASEURI}/events`)
+      if (response.status === 200) {
+        this.events = response.data;
+      }
     } catch (err) {
       console.log(err);
     }
