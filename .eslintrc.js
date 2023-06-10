0e24cb3fddd9fd2d9451e9fdb54cca4959054fa0
sericakitty <serica.suhonen@hotmail.com>
2023-06-10 11:42:45 +0300

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,24 +16,32 @@ module.exports = {
       'warn',
       'smart-tabs',
     ],
-    "semi": 'warn',
+    'semi': 'warn',
     'max-len': ['warn', { code: 500 }],
     'no-tabs': ['warn', { allowIndentationTabs: true }],
     'linebreak-style': ['warn', 'windows'],
     'eol-last': 'off',
-    'comma-dangle': 'warn',
-    'no-trailing-spaces': 'warn',
-    "indent": 0,
-    "import/prefer-default-export": "off",
+    'comma-dangle': 'off',
+    'no-trailing-spaces': ['warn'],
+    'indent': 0,
+    'import/prefer-default-export': 'off',
     'class-methods-use-this': 'warn',
-    "prefer-destructuring": ["error", {
-      "array": false,
-      "object": false
-    }],
-    "no-param-reassign": "warn",
-    "object-curly-newline": 0,
-    "quote-props": 0,
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": false, "optionalDependencies": false, "peerDependencies": false }],
+    'prefer-destructuring': [
+      'error', {
+      'array': false,
+      'object': false
+      }
+    ],
+    'no-param-reassign': 'warn',
+    'object-curly-newline': 0,
+    'quote-props': 0,
+    'import/no-extraneous-dependencies': [
+      'error', {
+        'devDependencies': false,
+        'optionalDependencies': false,
+        'peerDependencies': false
+      }
+    ],
   },
   settings: {
     'import/resolver': {
