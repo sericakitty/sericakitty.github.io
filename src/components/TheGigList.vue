52775c2fc15bc339e48af5d4630f0097a792855c
sericakitty <63978546+sericakitty@users.noreply.github.com>
2025-02-03 19:40:31 +0200

    refactor TheGigList.vue for improved code formatting and consistency

--- src/components/TheGigList.vue
+++ src/components/TheGigList.vue
@@ -1,12 +1,10 @@
 <template>
-  
+
   <section>
     <details :open="gigs.isOpen" v-for="gigs in reversedGigList" :key="gigs.id">
       <summary>
         <span id="labelShow">Show Up {{ gigs.year }} Gigs &#9660;</span>
-        <span id="labelHide" class="defaultHide"
-          >Hide Out {{ gigs.year }} Gigs &#9650;</span
-        >
+        <span id="labelHide" class="defaultHide">Hide Out {{ gigs.year }} Gigs &#9650;</span>
       </summary>
 
       <table>
@@ -40,53 +38,57 @@
 import { checkDateIsPast, formatDate } from "Helpers";
 
 export default {
-props: {
-  gigs: {
-    required: true,
-    type: Array,
+  props: {
+    gigs: {
+      required: true,
+      type: Array,
+    },
   },
-},
-computed: {
-  reversedGigList() {
-    const copyList = [...this.gigs].reverse() // Take copy from original list and reversed it
-    copyList.forEach((element) => {
-      element.giglist.reverse()
-      element.giglist.forEach((gig) => {
-        gig.isPast = checkDateIsPast(gig.date)
-        const date = formatDate(gig.date)
-        gig.day = date[0]
-        gig.month = date[1]
-        gig.year = date[2]
-      })
-    }) // Iterate all events in copylist and reverse them
-    return copyList
+  computed: {
+    reversedGigList() {
+      const copyList = [...this.gigs].reverse() // Take copy from original list and reversed it
+      copyList.forEach((element) => {
+        element.giglist.reverse()
+        element.giglist.forEach((gig) => {
+          gig.isPast = checkDateIsPast(gig.date)
+          const date = formatDate(gig.date)
+          gig.day = date[0]
+          gig.month = date[1]
+          gig.year = date[2]
+        })
+      }) // Iterate all events in copylist and reverse them
+      return copyList
+    },
   },
-},
-methods: {
-  checkDate(isPast) {
-    return isPast ? "past" : ""
+  methods: {
+    checkDate(isPast) {
+      return isPast ? "past" : ""
+    },
   },
-},
 }
 </script>
 
 <style>
 .date_td .day_span {
-display:inline-block;
-width: 1.1rem;
-text-align: right;
-margin-right: 0.5rem;
+  display: inline-block;
+  width: 1.1rem;
+  text-align: right;
+  margin-right: 0.3rem;
 }
 
 .date_td .month_span {
-display:inline-block;
-width: 2rem;
-text-align: left;
+  display: inline-block;
+  width: 2rem;
+  text-align: left;
 }
 
 .date_td .year_span {
-display:inline-block;
-width: 2.5rem;
-text-align: right;
+  display: inline-block;
+  width: 2.5rem;
+  text-align: left;
+}
+
+.past td {
+  color: #bcbcbc;
 }
 </style>
